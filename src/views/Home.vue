f1f42aa8ac57e07660972ecd4ded00f7e7f40d2c
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-01-17 18:04:11 +0100

    autocomplete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -45,19 +45,23 @@
         
         <v-col cols="12" lg="9">
           <v-app-bar class="mx-n3" style="border-bottom:1px solid grey;">
-            <v-tabs>
-              <v-tab>Item One</v-tab>
-              <v-tab>Item Two</v-tab>
-              <v-tab>Item Three</v-tab>
-            </v-tabs>
+            
+              <v-btn elevation="1" color="grey darken-3">
+                <span class="text-capitalize">Launchpad</span>
+                <v-icon class="pl-16">close</v-icon>
+              </v-btn>
+              <v-btn outlined class="ml-2">
+                <v-icon>add</v-icon>
+              </v-btn>
+              <v-btn outlined class="ml-2">
+                <v-icon>more_horiz</v-icon>
+              </v-btn>
 
             <v-spacer></v-spacer>
             <v-divider vertical></v-divider>
             <v-autocomplete class="ml-3"
               label="No Environment"
-              clearable
               hide-details
-              hide-selected
               solo dense>
             </v-autocomplete>
             <v-btn class="ml-3 mr-3"  elevation="1" color="grey darken-3">
