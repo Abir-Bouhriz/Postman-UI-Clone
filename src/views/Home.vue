cb50f2a8a4a18a09455646254ccca20f410037a3
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-01-18 13:11:44 +0100

    4th card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -177,27 +177,58 @@
                   </v-btn>
                 </div>
               </v-card>
-              <v-card outlined class="mt-3 pa-3">
+              <v-card outlined class="mt-5 mr-10 pa-3">
                 <h3 class="ml-5 mt-5 ">What's new with Postman</h3>
                 <h4 class="mt-5 ml-7">It's Rocket Fuel for Your API Skills<span class="ml-3 orange--text text--darken-3 px-1" style="border:1px solid #EF6C00">New</span></h4>
-                <p class="ml-7 mt-4 font-weight-light">Postman Galaxy is the global, virtual API conference whre you can engage with API professionals from all over the world.
+                <p class="ml-7 mt-4 grey--text font-weight-light">Postman Galaxy is the global, virtual API conference whre you can engage with API professionals from all over the world.
                   Attend live demos, ask questions, and connect with fellow attendees to expand your knowledge and network.
                 </p>
                 <v-btn class="ml-4 orange--text text--darken-3 custom-transform-class text-none"> Register now</v-btn>
                 <v-divider class="mx-5 mt-2"></v-divider>
                 <h4 class="mt-5 ml-7">The $100,000 Hackathon: Join the Postman API Hack <span class="ml-3 orange--text text--darken-3 px-1" style="border:1px solid #EF6C00">New</span></h4>
-                <p class="ml-7 mt-4 font-weight-light">$100,000 in prizes is up for grabs, Join the Postman API Hack, our first-ever global,
+                <p class="ml-7 mt-4 grey--text font-weight-light">$100,000 in prizes is up for grabs, Join the Postman API Hack, our first-ever global,
                   virtual hackathon, and submit your brilliant idea(s) for your chance to win.
                 </p>
                 <v-btn class="ml-4 orange--text text--darken-3 custom-transform-class text-none"> Join the hack</v-btn>
                 <v-divider class="mx-5 mt-2"></v-divider>
               </v-card>
-              <v-card outlined class="mt-3 pa-3">
+              <v-card outlined class="mt-5 mr-10 pa-3">
                 <h3 class="ml-5 mt-5 ">Join a team from your org</h3>
                 <p class="ml-5 mt-4 font-weight-light">You're not a part of a team. Create a new one to start collaborating.
                 </p>
                 <v-btn class="ml-2 orange--text text--darken-3 custom-transform-class text-none"> Invite people to collaborate with you</v-btn>
               </v-card>
+              <v-card outlined class="my-5 mr-10 pa-3">
+                <h3 class="ml-5 mt-5 ">Discover</h3>
+                <p class="ml-5 mt-4 font-weight-light">Explore some templates and public APIs you might find useful.
+                </p>
+                <div class="text-center mt-3">
+              <v-btn tile class="px-16 custom-transform-class text-none" style="border-bottom: 4px solid #EF6C00">
+                Templates 
+              </v-btn>
+              <v-btn tile class="px-16 custom-transform-class text-none">
+                APIs 
+              </v-btn>
+              <v-divider></v-divider>
+                </div>
+                 <h4 class="ml-5 mt-5 ">Intro to writing tests - with examples</h4>
+                <p class="ml-5 mt-4 grey--text font-weight-light">This collection contains examples of tests that you can use to automate your
+                   testing process. Includes basic test syntax, examples of API tests, and integration tests.
+                </p>
+                <v-divider></v-divider>
+                <h4 class="ml-5 mt-5 ">COVID19 API</h4>
+                <p class="ml-5 mt-4 grey--text font-weight-light">Corona Virus stats REST API documentation
+                </p>
+                <v-divider></v-divider>
+                <h4 class="ml-5 mt-5 ">Working with GraphQL
+</h4>
+                <p class="ml-5 mt-4 grey--text font-weight-light">CExamples of working with GraphQL endpoints such as using a JSON request body, content-type, and importing queries.
+                </p>
+                <v-divider></v-divider>
+                <div class="text-center">
+                  <v-btn class="mt-2 text-capitalize grey--text text--lighten-2">Browse More</v-btn>
+                </div>
+                </v-card>
             </v-col>
           </v-row>
         </v-col>
